880fb971de0881dff0e2aa37c9d0bb7648773ec6
Ogega254 <ogegagideon9@gmail.com>
2021-11-21 22:02:35 -0800

    13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,41 +1,35 @@
 #include "lists.h"
 
 /**
- * is_palindrome - determine if singly linked list is palindrome
- * @head: pointer to head of singly linked list
- * Return: 0 if not, 1 if palindrome
+ * is_palindrome - checks if a singly linked list is a palindrome.
+ * @head: pointer to the list.
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome or -1 (error).
  */
 int is_palindrome(listint_t **head)
 {
-  listint_t *tmp = *head;
-  unsigned int size = 0, i = 0;
-  int data[10240];
+	listint_t *aux;
+	int len, i, *copy;
 
-  if (head == NULL) /* non-existing list is not */
-    return (0);
-
-  if (*head == NULL) /* empty list is palindrome */
-    return (1);
-
-  while (tmp) /* find size of linked list */
-    {
-      tmp = tmp->next;
-      size += 1;
-    }
-  if (size == 1) /* single node list is palindrome */
-    return (1);
-
-  tmp = *head;
-  while (tmp) /* pull node data into array to compare */
-    {
-      data[i++] = tmp->n;
-      tmp = tmp->next;
-    }
-
-  for (i = 0; i <= (size/2); i++)
-    {
-      if (data[i] != data[size - i - 1])
-	return (0);
-    }
-  return (1);
+	/* empty list */
+	if (!*head || !head)
+		return (1);
+	/* calculate length of the list */
+	aux = *head;
+	for (len = 1; aux->next; len++)
+		aux = aux->next;
+	/* create a copy of the list in an array */
+	copy = malloc(sizeof(int) * (len));
+	if (!copy)
+		return (-1);
+	for (i = 0, aux = *head; i < len; i++, aux = aux->next)
+		copy[i] = aux->n;
+	/* checks if its a palindrome */
+	for (i = 0; i < (len / 2); i++)
+		if (copy[i] != copy[len - 1 - i])
+		{
+			free(copy);
+			return (0);
+		}
+	free(copy);
+	return (1);
 }
