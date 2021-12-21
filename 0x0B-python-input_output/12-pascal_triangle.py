6eab3eb14143e0e534ceddda251ba30ae4171c32
Ogega254 <ogegagideon9@gmail.com>
2021-12-21 11:31:11 -0800

    Pascal triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""Student to JSON"""
+"""Pascal's Triangle"""
 
 
-class Student:
-    """representation of a student"""
-    def __init__(self, first_name, last_name, age):
-        """instantiation of the student"""
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
-
-    def to_json(self):
-        """retrieves a dictionary representation of a Student instance"""
-        return self.__dict__
+def pascal_triangle(n):
+    """returns a list of lists of integers
+    representing the Pascal’s triangle of n"""
+    triangle = []
+    if n <= 0:
+        return []
+    for i in range(n):
+        a = 11 ** i
+        row = [int(digit) for digit in str(a)]
+        triangle += [row]
+    return triangle
