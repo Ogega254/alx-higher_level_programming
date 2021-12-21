9f3d62d39c74d5f3737e684580a510c8ff1f7733
Ogega254 <ogegagideon9@gmail.com>
2021-12-21 11:52:57 -0800

    12-pascal_triangle.py

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-''' Pascal's Triangle
-'''
+"""Pascal's Triangle"""
 
 
 def pascal_triangle(n):
-    '''module pascal_triangle
-    '''
-
-    def __init__(self, first_name, last_name, age):
-        '''method __init__
-        '''
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
+    """returns a list of lists of integers
+    representing the Pascal’s triangle of n"""
+    triangle = []
+    if n <= 0:
+        return []
+    for i in range(n):
+        a = 11 ** i
+        row = [int(digit) for digit in str(a)]
+        triangle += [row]
+    return triangle
