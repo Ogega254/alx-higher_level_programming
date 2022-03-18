d2a4923c82efaad4a09bc4f5ad70e8b5833cbe9d
Ogega254 <ogegagideon9@gmail.com>
2022-03-18 13:26:06 -0700

    What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-# Python script that fetches a url
-import urllib.request as ur
+"""
+Fetches https://intranet.hbtn.io/status
+use the package urllib
+ body of the response must be displayed in tabulation before -
+"""
+import urllib.request
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    html = response.read()
 
-with ur.urlopen('https://intranet.hbtn.io/status') as res:
-    res = res.read()
-
-print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(res), res))
-print('\t- utf8 content: {}'.format(str(res, 'utf-8')))
+print('Body response:\n\t- type: {}'.format(type(html)))
+print('\t- content: {}'.format(html))
+print('\t- utf8 content: {}'.format(html.decode('utf-8')))
