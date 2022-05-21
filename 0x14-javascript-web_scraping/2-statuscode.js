6c9d0fd28c24f882bcfed9e7bfe077015814821e
root <gideonogega9@gmail.com>
2022-05-21 12:25:35 +0000

    2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,5 @@
 #!/usr/bin/node
-/* displays the status code of a GET request.
-    - the first argument is the URL to request (GET).
-    - output format: 'code: <status code>'.
-*/
 const request = require('request');
-request(process.argv[2], function (error, response) {
-  if (error) console.log(error);
-  else console.log('code: ' + response.statusCode);
+request.get(process.argv[2]).on('response', function (response) {
+  console.log(`code: ${response.statusCode}`);
 });
