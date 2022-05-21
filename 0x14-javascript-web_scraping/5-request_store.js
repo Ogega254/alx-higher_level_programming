cbec347f3b9ad46b9911ada9bbfa44d96c081309
root <gideonogega9@gmail.com>
2022-05-21 12:31:03 +0000

    script request

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,15 +1,4 @@
 #!/usr/bin/node
-/* gets the contents of a webpage and stores it in a file.
-    - the first argument is the URL to request.
-    - the second argument the file path to store the body response.
-*/
-const request = require('request');
 const fs = require('fs');
-request(process.argv[2], function (error, response, body) {
-  if (error) console.log(error);
-  else {
-    fs.writeFile(process.argv[3], body, 'utf-8', function (error) {
-      if (error) console.log(error);
-    });
-  }
-});
+const request = require('request');
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,21 +1,14 @@
 #!/usr/bin/node
-/* computes the number of tasks completed by user id.
-    - the first argument is the API URL.
-    - only prints users with completed task.
-*/
 const request = require('request');
-const url = process.argv[2];
-request(url, function (error, response, body) {
-  if (error) console.log(error);
-  else if (response.statusCode === 200) {
-    const completed = {};
-    JSON.parse(body).forEach(task => {
-      if (task.completed === true) {
-        if (completed[task.userId] === undefined) {
-          completed[task.userId] = 1;
-        } else {
-          completed[task.userId]++;
-        }
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const todos = JSON.parse(body);
+    let completed = {};
+    todos.forEach((todo) => {
+      if (todo.completed && completed[todo.userId] === undefined) {
+        completed[todo.userId] = 1;
+      } else if (todo.completed) {
+        completed[todo.userId] += 1;
       }
     });
     console.log(completed);
